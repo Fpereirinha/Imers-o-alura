724491c132c253b650eeb873830153f5488ad454
Fpereirinha <pedrofpereira222@gmail.com>
2022-03-09 13:17:10 -0300

    att

--- mentalistaid3pedro/mentalistaid3pedro/src/script.js
+++ mentalistaid3pedro/mentalistaid3pedro/src/script.js
@@ -1,17 +1,22 @@
 var secret = Math.floor(Math.random() * 10 + 1);
 var cont = 0;
+var botao = document.getElementById("btn_chutar");
 console.log(secret);
+function desativaBotao() {
+  botao.disabled = true;
+}
 function Chutar() {
   var resultado = document.getElementById("resultado");
   var chute = parseInt(document.getElementById("valor").value);
-  console.log(chute);
   if (cont == 3) {
     resultado.innerHTML =
       "Você errou 3 vezes, tente novamente. O número era " + secret;
+    desativaBotao();
   } else {
     if (chute == secret) {
       cont += 1;
       resultado.innerHTML = "Você acertou com " + cont + " tentativas";
+      desativaBotao();
     } else if (chute > 10 || chute < 0) {
       resultado.innerHTML = "Número fora do alcance";
     } else {
@@ -24,6 +29,7 @@ function Chutar() {
       if (cont >= 3) {
         resultado.innerHTML =
           "Você errou 3 vezes, tente novamente. O número era " + secret;
+        desativaBotao();
       }
     }
   }
